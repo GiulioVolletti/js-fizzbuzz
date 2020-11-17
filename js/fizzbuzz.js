942071a858335c7b5b2fe1758d21e021ea3fd6df
Giulio <giuliovolletti@hotmail.com>
2020-11-17 15:32:40 +0100

    cambio layout

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -6,17 +6,17 @@ for (var i = 1; i < 101; i++) {
   // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
   if ((i % 5 == 0) && (i % 3 == 0)) {
     // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-    stampa += "FizzBuzz" + "<p> click here"  + "</p>";
+    stampa += "FizzBuzz" + "<p>" + "</p>";
     console.log("FizzBuzz");
   } else if (i % 5 == 0) {
     console.log("Buzz");
-    stampa += "Buzz" + "<p> click here"  + "</p>";
+    stampa += "Buzz" + "<p>" + "</p>";
   } else if (i % 3 == 0) {
     console.log("Fizz");
-    stampa += "Fizz" + "<p> click here"  + "</p>";
+    stampa += "Fizz" + "<p>" + "</p>";
   } else {
     console.log(i);
-    stampa += i + "<p> click here"  + "</p>";
+    stampa += i + "<p>" + "</p>";
   }
 }
 document.getElementById('log_stampato').innerHTML += stampa
